6f86836b815ebdb5668fc142b386b8a30af0892f
Alfred Mtambalika <karanlicka@gmail.com>
2026-02-11 07:43:11 +0530

    Counting unique characters in a given string

--- practice.py
+++ practice.py
@@ -1,3 +1,14 @@
 # option + fn + F12 to open the terminal in mac
+from random import seed
 
-print('Hello world')
+#Counting unique characters in a given string
+s = 'abca'
+seen_char = ''
+count = 0
+for char in s:
+    if char not in seen_char:
+        seen_char += char
+        count += 1
+print(count)
+
+
