18c4de4cd78540909635f94ace5cbc67357d9a1a
Alfred Mtambalika <karanlicka@gmail.com>
2026-02-13 18:15:34 +0530

    Concatenating strings

--- practice.py
+++ practice.py
@@ -1,10 +1,9 @@
 # option + fn + F12 to open the terminal in mac
 from random import seed
 
-#Counting unique characters in a given string
+
 
 # Day 4 of 30 days of python
-
-challenge = 'Thirty days of python'
-print(challenge.count('y',7,14))
+# concatenating strings
+print('Coding '  + 'For ' + 'All')
 
