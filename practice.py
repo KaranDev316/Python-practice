bf19700eb697537b94d9df5de2c045d5dad5947d
Alfred Mtambalika <karanlicka@gmail.com>
2026-02-14 23:08:38 +0530

    Displaying using string formatting

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
 # option + fn + F12 to open the terminal in mac
-from random import seed
-
-
 
 # Day 4 of 30 days of python
 # how to use len()
 
-company = "mahadev computers"
+# Displaying using string formating
 
-print(len(company))
+radius = 10
 
+area = 3.14 * radius ** 2
+
+print(f"The area of the circle with radius {radius} is {area}")
