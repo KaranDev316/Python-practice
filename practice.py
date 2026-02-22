9725e0e9f9b5cbafec441180c6c0f7c1234b303d
karanlicka <karanlicka@gmail.com>
2026-02-22 22:31:16 +0530

    Converting a tuple into a list

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 6 of 30 days of python
-# How to create a tuple
+# How to change a tuple to list
 
 empty_tuple =()
-print(type(empty_tuple))
+lst = list(empty_tuple)
 
-
+print(type(lst))
 
 
 
