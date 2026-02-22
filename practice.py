f6b394a3ff570cc0dac6413121be81317ce37473
karanlicka <karanlicka@gmail.com>
2026-02-22 22:29:24 +0530

    How to create a tuple

--- practice.py
+++ practice.py
@@ -1,16 +1,10 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 6 of 30 days of python
-#
-lst = []
-number = int(input("Enter a number: "))
-for i in range(1, number + 1):
-     num = int(int(input("Enter a number: ")))
-     lst.append(num)
+# How to create a tuple
 
-print(lst)
-
-
+empty_tuple =()
+print(type(empty_tuple))
 
 
 
