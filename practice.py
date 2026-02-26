afecaf8e7140d0edd082b08136dedf6f8edc4032
Alfred Mtambalika <karanlicka@gmail.com>
2026-02-26 23:16:45 +0530

    Creatin a set

--- practice.py
+++ practice.py
@@ -1,15 +1,10 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 7 of 30 days of python
+st = set('hello')
 
 
-empty_tuple =()
-lst = list(empty_tuple)
-
-print(type(lst))
-
-
-
+print(st)
 
 
 
