cfc17e19d395dd9b295f71661398ab1892102067
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-01 23:04:27 +0530

    using if, elif and else in condions

--- practice.py
+++ practice.py
@@ -1,8 +1,10 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 9 of python
-a = 3
+a = 0
 if a > 0:
     print("a is positive")
+elif a == 0:
+    print("a is zero")
 else:
     print("a is negative")
