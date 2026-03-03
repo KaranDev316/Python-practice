620f87d7948d6968d990548c7062f84e4da8399b
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-03 13:16:21 +0530

    Finding sum of even and old numbers from a range of 0 to 100

--- practice.py
+++ practice.py
@@ -1,12 +1,16 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 10 of python
-sums = 0
+sum_even = 0
+sum_odd = 0
 for i in range(101):
-    sums += i
-print(sums)
+    if i % 2 == 0:
+        sum_even += i
+    else:
+        sum_odd += i
 
-
+print(sum_even)
+print(sum_odd)
 
 
 
