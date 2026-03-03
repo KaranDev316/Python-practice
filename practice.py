fb25128f18e77420c0b96ca02ccd703b018cc748
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-03 13:20:53 +0530

    Extracting words that contains s in the list

--- practice.py
+++ practice.py
@@ -1,16 +1,11 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 10 of python
-sum_even = 0
-sum_odd = 0
-for i in range(101):
-    if i % 2 == 0:
-        sum_even += i
-    else:
-        sum_odd += i
+lst = ['Python', 'Numpy','Pandas','Django', 'Flask']
 
-print(sum_even)
-print(sum_odd)
+for i in lst:
+    if 's' in i:
+        print(i)
 
 
 
