8d29f480737f09c01ca34b791944d738faff8d9f
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-03 13:13:47 +0530

    Finding sum in range of 0 to 100

--- practice.py
+++ practice.py
@@ -1,11 +1,11 @@
 # option + fn + F12 to open the terminal in mac
 
 # Day 10 of python
+sums = 0
+for i in range(101):
+    sums += i
+print(sums)
 
-lst =  ['Python', 'Numpy','Pandas','Django', 'Flask']
-
-for i in lst:
-    print(i)
 
 
 
