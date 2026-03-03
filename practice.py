23fa084a168cda62eb05e955fe1116bab840e972
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-03 12:54:59 +0530

    Day 10 of 30 days of python

--- practice.py
+++ practice.py
@@ -1,14 +1,8 @@
 # option + fn + F12 to open the terminal in mac
 
-# Day 9 of python
+# Day 10 of python
 
-number1 = int(input("enter number 1: "))
-number2 = int(input("enter number 2: "))
 
-if number1 > number2:
-     print(f"{number1} is greater than  {number2}")
-else:
-     print(f"{number2} is greater than  {number1}")
 
 
 
