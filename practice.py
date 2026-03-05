b4c56b093f1736c59ec7a39349e1cf72a30c8c74
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-05 20:36:18 +0530

    Day 12 of 30 days of python

--- practice.py
+++ practice.py
@@ -2,14 +2,5 @@
 import math
 
 
-# Day 11 of python
+# Day 12 of python
 
-#creating functions
-
-def sum_numbers(num1):
-    sum = 0
-    for i in range(num1 + 1):
-        sum += i
-    print(sum)
-
-sum_numbers(100)
