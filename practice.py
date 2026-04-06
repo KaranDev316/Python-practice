4cee1f70f609423dc9afd16be407d8b1b27a3661
Alfred Mtambalika <karanlicka@gmail.com>
2026-04-06 12:11:12 +0530

    Calculating final price per given price and tax

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
-text = " python is fun "
-replaced = text.replace("fun", "awesome")
-print(replaced)
+price = int(input("Enter your price: "))
+tax = 10
+
+tax_calculation = price * tax/100
+final_price =price + tax_calculation
+print("Final: ", final_price)
