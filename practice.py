ebc87c057504219bddc32acddf63bb44a8467c75
Alfred Mtambalika <karanlicka@gmail.com>
2026-04-06 11:57:36 +0530

    Printing using formatting strings

--- practice.py
+++ practice.py
@@ -1,8 +1,5 @@
-# option + fn + F12 to open the terminal in mac
-import math
-
-
-# Day 12 of python
-
-# Modules in python
-
+name = "alfred"
+age = 21
+ist_student = True
+message = f"My name is {name} and I am {age} years old"
+print(message)
