58252fcf5670bc2b5db2b6e026b496d7ba8f6b33
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-01 11:04:43 +0530

    p_9 swapping two numbers without third valiable

--- practice.py
+++ practice.py
@@ -1,2 +1,14 @@
-name = input("what is your name?: ")
-print(len(name))
+number1 = 2
+number2 = 3
+print("Before  swapping two numbers")
+
+print("number 1 = ",number1)
+print("number 2 = ",number2)
+
+number1 = number1 + number2
+number2 =  number1 - number2
+number1 = number1 - number2
+
+print("AFTER swapping two numbers")
+print("number 1 = ",number1)
+print("number 2 = ",number2)
