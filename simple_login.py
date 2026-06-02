365b089143c3d16449c53d84a00f46609690a570
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-02 12:23:16 +0530

    improving number of attempts for better usability

--- simple_login.py
+++ simple_login.py
@@ -2,7 +2,8 @@ defined_username = "admin"
 defined_password = "abc123"
 
 for i in range(1,4):
-    print(i, "attempt(s)")
+    print("Attempt " + str(i) +" of 3")
+
     username = input("Please enter your username: ")
     password = input("Please enter your password: ")
     if username == defined_username and password == defined_password:
@@ -10,5 +11,4 @@ for i in range(1,4):
         break
     else:
         if i == 3:
-            print("You have reached the limit of 3 attempts")
-        i = i + 1
+            print("You have reached the maximum limit of 3 attempts")
