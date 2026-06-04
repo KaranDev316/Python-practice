73ff7ff32c0346349f79c4cb6cd9eebdc842704d
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-04 11:10:58 +0530

    count presence of both names in love

--- love_calculator.py
+++ love_calculator.py
@@ -1,6 +1,6 @@
 first_fullname = input("Please enter your first name: ").lower()
 second_fullname = input("Please enter your last name: ").lower()
-
+#count number of presence in true
 both_names = first_fullname +second_fullname
 t_count = both_names.count("t")
 r_count = both_names.count("r")
@@ -8,5 +8,15 @@ u_count = both_names.count("u")
 e_count = both_names.count("e")
 
 true_count = t_count + r_count + u_count + e_count
+
+#count number of presence in love
+l_count = both_names.count("l")
+o_count = both_names.count("o")
+v_count = both_names.count("v")
+e_count = both_names.count("e")
+
+love_count = l_count + o_count + v_count + e_count
+
 print(true_count)
+print(love_count)
 print(both_names)
