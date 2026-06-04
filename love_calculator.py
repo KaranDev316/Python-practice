0d9c2ec5ebfc44c13ad1063d8f2f19fd94823ec4
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-04 11:52:06 +0530

    changing the logic to simplified version

--- love_calculator.py
+++ love_calculator.py
@@ -1,30 +1,36 @@
 your_fullname = input("Please enter your  name: ").lower()
 partners_fullname = input("Please enter your partner's name: ").lower()
-
+true_count = 0
+love_count = 0
 #count the number presence of both names  in true
 both_names = your_fullname + partners_fullname
-t_count = both_names.count("t")
-r_count = both_names.count("r")
-u_count = both_names.count("u")
-e_count = both_names.count("e")
-
-true_count = t_count + r_count + u_count + e_count
+#t_count = both_names.count("t")
+#r_count = both_names.count("r")
+#u_count = both_names.count("u")
+for i in "true":
+    true_count +=both_names.count(i)
+#true_count = t_count + r_count + u_count + e_count
 
 #count the number presence of both names in love
-l_count = both_names.count("l")
-o_count = both_names.count("o")
-v_count = both_names.count("v")
-e_count = both_names.count("e")
+for i in "love":
+    love_count +=both_names.count(i)
+#l_count = both_names.count("l")
+#o_count = both_names.count("o")
+#v_count = both_names.count("v")
+#e_count = both_names.count("e")
 
-love_count = l_count + o_count + v_count + e_count
+#love_count = l_count + o_count + v_count + e_count
 score  = int(str(true_count) + str(love_count))
 
-print("Your matching score is "+ str(score) + "%")
-
-if score <= 40:
-    print("low score")
-elif score >= 50:
-    print("Middle range score")
+print(f"Your matching score is  {score} %")
+if score >= 80:
+    print("Very high score")
 elif score >= 70:
     print("High score")
+elif score >= 50:
+    print("Middle score")
+else:
+    print("low score")
+
+
 
