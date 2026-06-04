cb6ef42184b753149b5fee619df1c618e34bcfc6
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-04 12:04:49 +0530

    mvp

--- love_calculator.py
+++ love_calculator.py
@@ -4,22 +4,14 @@ true_count = 0
 love_count = 0
 #count the number presence of both names  in true
 both_names = your_fullname + partners_fullname
-#t_count = both_names.count("t")
-#r_count = both_names.count("r")
-#u_count = both_names.count("u")
+
 for i in "true":
     true_count +=both_names.count(i)
-#true_count = t_count + r_count + u_count + e_count
 
 #count the number presence of both names in love
 for i in "love":
-    love_count +=both_names.count(i)
-#l_count = both_names.count("l")
-#o_count = both_names.count("o")
-#v_count = both_names.count("v")
-#e_count = both_names.count("e")
+    love_count += both_names.count(i)
 
-#love_count = l_count + o_count + v_count + e_count
 score  = int(str(true_count) + str(love_count))
 
 print(f"Your matching score is  {score} %")
