9fc6f81d0de7e7b8fbaf4dd086928b704eeaaa4c
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-04 11:13:23 +0530

    basic functionality

--- love_calculator.py
+++ love_calculator.py
@@ -1,6 +1,6 @@
 first_fullname = input("Please enter your first name: ").lower()
 second_fullname = input("Please enter your last name: ").lower()
-#count number of presence in true
+#count number of both names presence in true
 both_names = first_fullname +second_fullname
 t_count = both_names.count("t")
 r_count = both_names.count("r")
@@ -9,7 +9,7 @@ e_count = both_names.count("e")
 
 true_count = t_count + r_count + u_count + e_count
 
-#count number of presence in love
+#count number of both names presence in love
 l_count = both_names.count("l")
 o_count = both_names.count("o")
 v_count = both_names.count("v")
@@ -17,6 +17,5 @@ e_count = both_names.count("e")
 
 love_count = l_count + o_count + v_count + e_count
 
-print(true_count)
-print(love_count)
+print(str(true_count) + str(love_count) + "%")
 print(both_names)
