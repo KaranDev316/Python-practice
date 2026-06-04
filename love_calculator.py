af3a1e05c59928928fd0cf2c5c1b6ae5545dcc20
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-04 11:16:23 +0530

    rephrasing the comment

--- love_calculator.py
+++ love_calculator.py
@@ -1,6 +1,7 @@
 first_fullname = input("Please enter your first name: ").lower()
 second_fullname = input("Please enter your last name: ").lower()
-#count number of both names presence in true
+
+#count the number presence of both names  in true
 both_names = first_fullname +second_fullname
 t_count = both_names.count("t")
 r_count = both_names.count("r")
@@ -9,7 +10,7 @@ e_count = both_names.count("e")
 
 true_count = t_count + r_count + u_count + e_count
 
-#count number of both names presence in love
+#count the number presence of both names in love
 l_count = both_names.count("l")
 o_count = both_names.count("o")
 v_count = both_names.count("v")
@@ -17,5 +18,4 @@ e_count = both_names.count("e")
 
 love_count = l_count + o_count + v_count + e_count
 
-print(str(true_count) + str(love_count) + "%")
-print(both_names)
+print("Your matching score is "+ str(true_count) + str(love_count) + "%")
