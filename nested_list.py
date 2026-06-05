71abdbe8cb7530841070b1536c78771384fb2a1c
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-05 14:45:26 +0530

    set,list and tuple

---
+++ nested_list.py
@@ -0,0 +1,12 @@
+#List set and tuples
+
+list1 = [1, 2, 3]
+
+tuple1 = (1, 2, 3)
+
+
+set1 = {1, 2, 3}
+
+print(f" example of a list is {list1}")
+print(f" example of a tuple is {tuple1}")
+print(f" example of a set  is {set1}")
