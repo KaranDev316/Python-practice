c97cbde440a8655d33e243ff129c1f193becb31d
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-05 11:51:48 +0530

    change of logic

--- random_name.py
+++ random_name.py
@@ -1,7 +1,10 @@
 import random
 
 names = (input("Enter everybody's name separated by a comma: ")).split(",")
-random_name = random.choice(names)
+
+random_int = random.randint(0, len(names)-1)
+
+random_name = names[random_int]
 print(f"{random_name} will pay the bill")
 
 
