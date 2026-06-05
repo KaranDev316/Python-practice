84d04c178e093b3a6df98a708b862009b8d126b8
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-05 11:44:37 +0530

    full implementation of the program

--- random_name.py
+++ random_name.py
@@ -1,6 +1,7 @@
 import random
 
 names = (input("Enter everybody's name separated by a comma: ")).split(",")
+random_name = random.choice(names)
+print(f"{random_name} will pay the bill")
 
-print(names)
 
