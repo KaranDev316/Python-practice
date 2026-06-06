4a96f9524a5427b073d8f2370fa48760fbef112c
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-06 15:20:17 +0530

    changes

--- max_number_of_given_list.py
+++ max_number_of_given_list.py
@@ -2,7 +2,7 @@ list1 = [19,22,93,64,45,62,7,18,9,10]
 
 maximum = list1[0]
 
-for i in range(1,len(list1)):
-    if list1[i] > maximum:
-        maximum = list1[i]
+for number in list1:
+    if number > maximum:
+        maximum = number
 print(maximum)
---
+++ test.py
@@ -0,0 +1,6 @@
+count = 0
+yourname = "friendship"
+for i in "alfred":
+    count +=yourname.count(i)
+print(count)
+
