4313bd24c5794e501e41fe34f71d216f71237695
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-07 16:49:01 +0530

    adding a tuple to a list with mutiple dictionaries

--- dictionary.py
+++ dictionary.py
@@ -13,5 +13,13 @@ student_data = [
      "course" : "Java",
 
      }
+
 ]
+new_entry_data = ("Shyam", 22 ,18 , "c++")
+
+new_dictionary = {"name": new_entry_data[0], "roll_no": new_entry_data[1], "age": new_entry_data[2]}
+
+student_data.append(new_dictionary)
+
+
 print(student_data)
