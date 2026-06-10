784fa27fe5094d355c1bc8d3794cb599377baa1e
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-10 10:23:00 +0530

    use function to generate the password

--- Python-Bootcamp/password_generator_project.py
+++ Python-Bootcamp/password_generator_project.py
@@ -2,17 +2,21 @@ import random
 import string
 import secrets
 
-number_of_letters = int(input("How many letters would you like?: "))
-number_of_symbols =int(input("How many symbols would you like?: "))
-number_of_digits = int(input("How many digits would you like?: "))
 
-password = []
-for i in range(number_of_letters):
-    password.append(secrets.choice(string.ascii_letters))
-for i in range(number_of_symbols):
-    password.append(secrets.choice(string.punctuation))
-for i in range(number_of_digits):
-      password.append(str(secrets.randbelow(10)))
+def generate_password(letters, symbols, digits):
+    password = []
+    for i in range(letters):
+        password.append(secrets.choice(string.ascii_letters))
+    for i in range(symbols):
+        password.append(secrets.choice(string.punctuation))
+    for i in range(digits):
+          password.append(str(secrets.randbelow(10)))
 
-random.shuffle(password)
-print(''.join(password))
+    random.shuffle(password)
+    print(''.join(password))
+
+(generate_password(
+        int(input("How many letters would you like?: ")),
+        int(input("How many symbols would you like?: ")),
+        int(input("How many digits would you like?: "))
+))
