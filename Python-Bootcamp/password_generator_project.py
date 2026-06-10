0baddc5a1dbe789713d2ef8487bb227e618c0ae2
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-10 10:17:53 +0530

    use secrests instead of random

--- Python-Bootcamp/password_generator_project.py
+++ Python-Bootcamp/password_generator_project.py
@@ -1,22 +1,18 @@
 import random
 import string
+import secrets
 
 number_of_letters = int(input("How many letters would you like?: "))
 number_of_symbols =int(input("How many symbols would you like?: "))
 number_of_digits = int(input("How many digits would you like?: "))
 
-letters = ""
-symbols = ""
-digits =""
-
+password = []
 for i in range(number_of_letters):
-    letters += random.choice(string.ascii_letters)
+    password.append(secrets.choice(string.ascii_letters))
 for i in range(number_of_symbols):
-    symbols += random.choice(string.punctuation)
+    password.append(secrets.choice(string.punctuation))
 for i in range(number_of_digits):
-      digits +=str(random.randint(0, 9))
+      password.append(str(secrets.randbelow(10)))
 
-
-password = list(letters + symbols + digits)
 random.shuffle(password)
-print("".join(password))
+print(''.join(password))
