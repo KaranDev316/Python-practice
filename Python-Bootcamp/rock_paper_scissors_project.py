03c236966da4e7427361be9c5c52c16ccbdccd78
karanlicka <karanlicka@gmail.com>
2026-06-11 12:54:57 +0530

    add validation block

--- Python-Bootcamp/rock_paper_scissors_project.py
+++ Python-Bootcamp/rock_paper_scissors_project.py
@@ -1,8 +1,19 @@
 import random
-user_choice = input("Enter your choice(Rock/Paper/Scissor): ")
+user_choice = input("Enter your choice(Rock/Paper/Scissors): ")
 
 choices = ["rock","paper","scissors"]
 
 computer_choice = random.choice(choices)
-
-print(computer_choice)
+if user_choice.lower() == "rock" or user_choice == "paper" or user_choice == "scissors":
+    if user_choice.lower() == computer_choice:
+        print("Draw")
+    elif user_choice.lower() == "rock" and computer_choice == "scissors":
+        print("You won")
+    elif user_choice.lower() == "scissors" and computer_choice == "paper":
+        print("You won")
+    elif user_choice.lower() == "paper" and computer_choice == "rock":
+        print("you won")
+    else:
+        print("YOU LOSE!")
+else:
+    print("Please enter Rock or Paper or Scissors")
