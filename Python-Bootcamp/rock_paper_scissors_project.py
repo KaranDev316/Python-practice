1f7698bab22ef5225b6c7fc028bc62405894f835
karanlicka <karanlicka@gmail.com>
2026-06-11 13:00:41 +0530

    convert user choice to lower on input

--- Python-Bootcamp/rock_paper_scissors_project.py
+++ Python-Bootcamp/rock_paper_scissors_project.py
@@ -1,17 +1,17 @@
 import random
-user_choice = input("Enter your choice(Rock/Paper/Scissors): ")
+user_choice = input("Enter your choice(Rock/Paper/Scissors): ").lower()
 
 choices = ["rock","paper","scissors"]
 
 computer_choice = random.choice(choices)
-if user_choice.lower() == "rock" or user_choice.lower() == "paper" or user_choice.lower() == "scissors":
-    if user_choice.lower() == computer_choice:
+if user_choice == "rock" or user_choice == "paper" or user_choice == "scissors":
+    if user_choice == computer_choice:
         print("Draw")
-    elif user_choice.lower() == "rock" and computer_choice == "scissors":
+    elif user_choice == "rock" and computer_choice == "scissors":
         print("You won")
-    elif user_choice.lower() == "scissors" and computer_choice == "paper":
+    elif user_choice == "scissors" and computer_choice == "paper":
         print("You won")
-    elif user_choice.lower() == "paper" and computer_choice == "rock":
+    elif user_choice == "paper" and computer_choice == "rock":
         print("you won")
     else:
         print("YOU LOSE!")
