7003a34923674b64de73e9076a0ecd9998d3c9b7
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-16 10:35:20 +0530

    testing objects,methods, calling methods

--- Python-Bootcamp/test.py
+++ Python-Bootcamp/test.py
@@ -1,3 +1,16 @@
-from test_package.utils import my_function
-print(my_function())
+class Car:
+    pass
 
+    def name_car(self, name):
+        self.name = name
+        print(f"This is my {name}")
+
+# Your code here
+car1 = Car()
+car2 = Car()
+
+
+print(car1.name_car("mahindra"))
+print(car2.name_car("Toyota"))
+
+# Output should show three DIFFERENT memory addresses
