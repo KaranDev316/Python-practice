77d1c4d37c654d65aebc8feb28e82e6646248d6b
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-17 17:33:36 +0530

    multilevel inheritance

---
+++ Python-Bootcamp/OOP/multilevel_inheritance.py
@@ -0,0 +1,14 @@
+class GrandParent:
+    def work(self):
+        print("GrandParent work")
+
+class Parent(GrandParent):
+    def work(self):
+        print("Parent work")
+
+class Child(Parent):
+    def work(self):
+        print("Child work")
+
+alfred = Child()
+alfred.work()
