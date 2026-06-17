db3f99a4187aef622500623e2e82434575637c64
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-17 16:16:14 +0530

    create class and object

--- Python-Bootcamp/test.py
+++ Python-Bootcamp/test.py
@@ -1,23 +1,15 @@
 #exercise 4: Bank Account
 
-class BankAccount:
-    def __init__(self,balance):
-        self.balance = balance
+class Student:
+    pass
 
-    def deposit(self,amount):
-        self.balance += amount
-        print(f"You have received ${amount}  and your balance is ${self.balance}")
+book = Student()
+book.title = "Computer visionary"
+book.author = "Andrej karpathy"
+book.pages = 34
 
-    def withdraw(self,amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            print(f"You have withdrawn ${amount}  and your balance is ${self.balance}")
-        else:
-            print(f"Sorry, you have insufficient balance")
-    def get_balance(self):
-        return self.balance
+print(book.pages)
+print(book.author)
+print(book.title)
 
 
-alfred = BankAccount(100)
-
-print(f"Your account balance is {alfred.get_balance()}")
