f57d44eff75d4db926cf571706a76219d89ac252
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-18 16:38:35 +0530

    finding items in a list

--- Python-Bootcamp/test.py
+++ Python-Bootcamp/test.py
@@ -1,2 +1,7 @@
-name = "alfred"
-print(name)
+my_list = [1,2,3,4,5,6,7,8,9]
+search = int(input("Enter your search term: "))
+
+if search in my_list:
+    print("found")
+else:
+    print("not found")
