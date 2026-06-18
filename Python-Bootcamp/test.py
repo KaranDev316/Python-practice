53b0aeee004c006da2cb272938694e3778442f7f
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-18 15:47:07 +0530

    NameError

--- Python-Bootcamp/test.py
+++ Python-Bootcamp/test.py
@@ -1,15 +1,2 @@
-#exercise 4: Bank Account
-
-class Student:
-    pass
-
-book = Student()
-book.title = "Computer visionary"
-book.author = "Andrej karpathy"
-book.pages = 34
-
-print(book.pages)
-print(book.author)
-print(book.title)
-
-
+name = alfred;
+print(name)
