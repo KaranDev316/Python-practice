81d46b1e0bdcd3d2d225a4ce18d78697d6e2cee6
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-23 16:56:14 +0530

    use else block when no exception is catched

---
+++ Python-Bootcamp/test.py
@@ -0,0 +1,9 @@
+
+try:
+    items = [1, 2, 3]
+    print(items[1])  # Triggering an IndexError
+
+except IndexError:
+    print("That item is not in the list")
+else:
+    print("That item is in the list")
