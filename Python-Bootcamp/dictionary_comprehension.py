67e47433d974992aefa04edccac81fb94fa06fee
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-27 12:46:50 +0530

    using condtional statement on dictionary comprehension

--- Python-Bootcamp/dictionary_comprehension.py
+++ Python-Bootcamp/dictionary_comprehension.py
@@ -1,12 +1,11 @@
-cities_in_F = {"New York":32, "Los Angeles":74, "Boston":100}
+# EXERCISE ON DICTIONARY COMPREHENSION
 
-cities_in_C = {key: round((value -32) *(5/9)) for key, value in cities_in_F.items()}
-print(cities_in_C)
+weather = {
+      "New York": "snowing",
+      "Boston": "sunny",
+      "los angeles": "sunny"
+}
 
-#Withought Dictionary comprehension it will look like this
-dict_2 = {}
-for key, value in cities_in_C.items():
-    value = round((value -32) *(5/9))
-    dict_2[key] = value
+sunny_weather = {key:value for (key, value) in weather.items() if value == "sunny"}
 
-print(dict_2)
+print(sunny_weather)
