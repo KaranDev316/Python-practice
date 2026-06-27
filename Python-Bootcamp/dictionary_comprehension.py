14b46ba841eac72cd4adf9b7e51969dc12d926ee
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-27 13:12:30 +0530

    using function inside dictionary comprehension

--- Python-Bootcamp/dictionary_comprehension.py
+++ Python-Bootcamp/dictionary_comprehension.py
@@ -9,3 +9,23 @@ weather = {
 sunny_weather = {key:value for (key, value) in weather.items() if value == "sunny"}
 
 print(sunny_weather)
+
+#Use of if/else in dictionary comprehension
+def weather_condition_check(value):
+    if value < 20:
+        return "Cold"
+    elif value <= 30:
+        return "Warm"
+    else:
+        return "Hot"
+
+weather = {
+      "New York": 30,
+      "Boston": 25,
+      "los angeles": 14
+}
+
+weather_condition = {key: ("Cold" if value < 25 else "Hot") for (key, value) in weather.items()}
+weather_condition2 = {key: weather_condition_check(value) for (key, value) in weather.items()}
+
+print(weather_condition2)
