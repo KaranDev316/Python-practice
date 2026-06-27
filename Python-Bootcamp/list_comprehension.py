f98c6f0146c8fbfc63af2c1b8ea845f863ae1e3e
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-27 11:54:14 +0530

    printing double using normal list

--- Python-Bootcamp/list_comprehension.py
+++ Python-Bootcamp/list_comprehension.py
@@ -1,11 +1,6 @@
-animals = ["lion", "tiger", "monkey", "elephant", "frog"]
-filtered_animals = []
+# Exercise 2 on list comprehension
+doubles = []
+for i in range(1, 11):
+    doubles.append(i * 2)
+print(doubles)
 
-for animal in animals:
-    filtered_animals.append(animal.title())
-
-print(filtered_animals)
-# list comprehension version
-animals = ["lion", "tiger", "monkey", "elephant", "frog"]
-filtered_animals = [animal.title() for animal in animals]
-print(filtered_animals)
