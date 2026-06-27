c24d1c3a14d1355473e6df931df4a6c68e8663cc
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-27 12:40:10 +0530

    dictionary comprehension and normal dictionary

--- Python-Bootcamp/dictionary_comprehension.py
+++ Python-Bootcamp/dictionary_comprehension.py
@@ -2,3 +2,9 @@ cities_in_F = {"New York":32, "Los Angeles":74, "Boston":100}
 
 cities_in_C = {key: round((value -32) *(5/9)) for key, value in cities_in_F.items()}
 print(cities_in_C)
+
+#Withought Dictionary comprehension it will look like this
+
+for key, value in cities_in_C.items():
+    value = round((value -32) *(5/9))
+    print(key, value)
