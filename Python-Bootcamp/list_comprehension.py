de34af956baa8925cf308ef8374bdbf074abaa8e
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-27 11:49:58 +0530

    converting a normal list to list comprehension technique

--- Python-Bootcamp/list_comprehension.py
+++ Python-Bootcamp/list_comprehension.py
@@ -5,3 +5,7 @@ for animal in animals:
     filtered_animals.append(animal.title())
 
 print(filtered_animals)
+# list comprehension version
+animals = ["lion", "tiger", "monkey", "elephant", "frog"]
+filtered_animals = [animal.title() for animal in animals]
+print(filtered_animals)
