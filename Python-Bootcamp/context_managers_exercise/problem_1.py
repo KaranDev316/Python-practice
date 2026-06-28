b2d34acf69af3a0edaf8566e0f83d5e1a9a36529
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-28 15:13:49 +0530

    finaly answer for problem 1

--- Python-Bootcamp/context_managers_exercise/problem_1.py
+++ Python-Bootcamp/context_managers_exercise/problem_1.py
@@ -1,5 +1,16 @@
-with open("cities.txt", "w") as file:
+
+# creating a file and writing list of city names
+with open("cities.txt","w") as file:
     file.write("Tokyo \n")
     file.write("Paris \n")
     file.write("New York \n")
     file.write("Sydney \n")
+
+# reading the list of countries and counting them
+with open("cities.txt", "r") as f:
+    file1 = f.read().splitlines()
+    count = 0
+    for city in file1:
+        count += 1
+
+print(count)
