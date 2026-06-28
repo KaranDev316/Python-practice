7b389d09125012ffa4bcf15dd9f5b677604ba47b
Alfred Mtambalika <karanlicka@gmail.com>
2026-06-28 14:54:03 +0530

    creating cities list

---
+++ Python-Bootcamp/context_managers_exercise/problem_1.py
@@ -0,0 +1,5 @@
+with open("cities.txt", "w") as file:
+    file.write("Tokyo \n")
+    file.write("Paris \n")
+    file.write("New York \n")
+    file.write("Sydney \n")
--- Python-Bootcamp/test.py
+++
@@ -1,5 +0,0 @@
-def load_user()->dict[str,list[int]]:
-    return{
-        "Alice": [1,2],
-        "Bob": [3]
-    }
