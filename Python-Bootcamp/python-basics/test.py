f4cfbf947a4152bc1d1b73503c623938b71daa31
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-04 09:40:58 +0530

    create dictionary

--- Python-Bootcamp/python-basics/test.py
+++ Python-Bootcamp/python-basics/test.py
@@ -1,5 +1,5 @@
 student_scores = { 'Tom': 85, 'Serena': 92, 'Alex': 78, 'Nina': 88 }
 
-# printing the scores
+# printing the key - value pairs
 for student, score in student_scores.items():
     print(f"{student}: {score}")
