879b5c4e8458f6807cf5385b69e06cbcb624bb72
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-05 15:13:33 +0530

    create get issue route

--- Python-Bootcamp/dsa/majority_vote_problem.py
+++ Python-Bootcamp/dsa/majority_vote_problem.py
@@ -1,6 +1,9 @@
 
 
-nums = {3,2,3}
-print(dict.get(nums, 0))
-print(dict.get(nums, 1))
-print(dict.get(nums, 2))
+nums = [3,2,3]
+dict_1 = {}
+
+for num in nums:
+    dict_1[num] = dict_1.get(num, 0) + 1
+
+print(dict_1)
--- Python-Bootcamp/my-fastapi-practice/issue-tracker/app/routes/issues.py
+++ Python-Bootcamp/my-fastapi-practice/issue-tracker/app/routes/issues.py
@@ -1,10 +1,10 @@
-
 import uuid
 from fastapi import APIRouter, HTTPException, status
-from app.schemas import IssueCreate, IssueUpdate, IssueOut, IssueStatus
+from app.schemas import IssueCreate, IssueUpdate, IssueOut
 from app.storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags=["Issues"])
+
 
 @router.get("", response_model=list[IssueOut])
 def get_issues():
