8590ba85ada3ccd8580b8b7f22be1173eccd2fab
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-07 18:32:04 +0530

    create note route

--- Python-Bootcamp/my-fastapi-practice/notes-api/app/routes/notes.py
+++ Python-Bootcamp/my-fastapi-practice/notes-api/app/routes/notes.py
@@ -1,5 +1,26 @@
+import uuid
 from fastapi import APIRouter, HTTPException, status
-
+from ..schemas.note import NoteCreate, NoteUpdate,NoteResponse
+from ..storage.note_storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags=["Issues"])
 
+@router.post("", response_model=NoteResponse, status_code=status.HTTP_201_CREATED)
+def create_issue(payload: NoteCreate):
+    """
+    Create new issue
+    The issue is persisted to data/issues.json
+    """
+    issues = load_data()
+
+    issue = {
+        "id": str(uuid.uuid4()),
+        "title": payload.title,
+        "description": payload.body,
+
+    }
+
+    issues.append(issue)
+    save_data(issues)
+
+    return issue
