8a9daa7476943c9dfbae4c4ada5eae4f3b7b2b9b
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-10 11:14:27 +0530

    converting a 2d list to 1d using extend

--- Python-Bootcamp/dsa/2d_intersection.py
+++ Python-Bootcamp/dsa/2d_intersection.py
@@ -1,14 +1,12 @@
 from collections import Counter
 nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
 
-nums_count = Counter(set(nums))
-
-result1 = []
-
-
+result_counter = []
+nums_extend = []
 for i in range(len(nums)):
-    print(i)
-    print(set(nums[i]))
-    result1.append(set(nums[i]))
+    nums_extend.extend(set(nums[i]))
+
+print(nums_extend)
+
 
 
