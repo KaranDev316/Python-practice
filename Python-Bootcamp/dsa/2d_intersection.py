7d790ba93d984d70a6ff25084192a89c6f756e6b
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-11 10:23:52 +0530

    leetcode 2248

--- Python-Bootcamp/dsa/2d_intersection.py
+++ Python-Bootcamp/dsa/2d_intersection.py
@@ -1,12 +1,16 @@
 from collections import Counter
 nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
-
-result_counter = []
+result = []
 nums_extend = []
 for i in range(len(nums)):
     nums_extend.extend(set(nums[i]))
 
-print(nums_extend)
 
+nums_count = Counter(nums_extend)
+print(nums_count)
 
+for key,num in nums_count.items():
+    if num == len(nums):
+        result.append(key)
 
+print(result)
