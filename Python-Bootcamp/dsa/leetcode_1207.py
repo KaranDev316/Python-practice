67724e206f6045f84869faffdac62865ad1b0223
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-14 09:36:11 +0530

    finding unique occurancy

--- Python-Bootcamp/dsa/leetcode_1207.py
+++ Python-Bootcamp/dsa/leetcode_1207.py
@@ -1,9 +1,14 @@
 from collections import Counter
-arr = [2,2,1,1,3]
+arr = [1,2,2,1,1,3]
 result  = set()
 counts = Counter(arr)
+items = []
 
-for key, item in counts.items():
+for _, item in counts.items():
     result.add(item)
+    items.append(item)
 
-print(result)
+if len(result) == len(items):
+    print("Unique items in list")
+else:
+    print("Not Unique items in list")
