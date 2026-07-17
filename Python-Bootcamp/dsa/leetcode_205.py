cb310948e59420460bc83b48bf2c722aaf26af15
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-17 09:01:44 +0530

    cheking the length after excluding the duplicates

--- Python-Bootcamp/dsa/leetcode_205.py
+++ Python-Bootcamp/dsa/leetcode_205.py
@@ -1,6 +1,8 @@
 
-s = "egg"
-t = "add"
-s = "f11", t = "b23"
-source_map_1 = dict(zip(s,t))
-print(source_map_1)
+
+j = "f11"
+k= "b23"
+
+source_map_2 = dict(zip(j,k))
+if len(source_map_2) == len(set(j)) and len(source_map_2) == len(set(k)):
+    print(True)
