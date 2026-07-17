b6fc8c77c2d933783807054d2d33fbbe737eb515
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-17 09:06:52 +0530

    working version

--- Python-Bootcamp/dsa/leetcode_205.py
+++ Python-Bootcamp/dsa/leetcode_205.py
@@ -2,7 +2,14 @@
 
 j = "f11"
 k= "b23"
+longest_string = 0
+if len(j) > len(k):
+    longest_string = len(j)
+else:
+    longest_string = len(k)
 
 source_map_2 = dict(zip(j,k))
-if len(source_map_2) == len(set(j)) and len(source_map_2) == len(set(k)):
+if len(source_map_2) == longest_string:
     print(True)
+else:
+    print(False)
