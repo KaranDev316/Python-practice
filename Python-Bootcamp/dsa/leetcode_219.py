4aa0b7aedfecb71c4a57769709220bd4c0ed3826
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-18 10:08:19 +0530

    valid sodoku problem

--- Python-Bootcamp/dsa/leetcode_219.py
+++ Python-Bootcamp/dsa/leetcode_219.py
@@ -1,11 +1,19 @@
 from collections import defaultdict
 nums = [1,0,1,1]
 k = 1
+seen = defaultdict(int)
 
-groups = defaultdict(list)
+for i, num in enumerate(nums):
+    if num in seen:
+        if abs(seen[num] -  i) <= k:
+            print(True)
+    seen[num] = i
 
-for i, item in enumerate(nums):
-    groups[item].append(i)
 
-for item in groups:
-    print(groups[item])
+
+
+
+
+
+
+
