2291ff4ee33b8b1b2f0c2bcca34841cbf6d5b753
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-21 09:44:54 +0530

    all test cases passed

--- Python-Bootcamp/dsa/leetcode_128.py
+++ Python-Bootcamp/dsa/leetcode_128.py
@@ -1,4 +1,4 @@
-nums = [100, 4, 200, 1, 3, 2]
+nums = [0,3,7,2,5,8,4,6,0,1]
 
 set_nums = set(nums)
 sequence = set()
@@ -7,5 +7,4 @@ for num in nums:
     if (num - 1 in nums) or (num + 1 in nums):
         sequence.add(num)
 
-print(sequence)
-print(set_nums)
+print(len(sequence))
