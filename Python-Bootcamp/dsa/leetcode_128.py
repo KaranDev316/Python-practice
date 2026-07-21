39f4d5d0cf63ef83402cb7e5efb2eb32e83ae61c
Alfred Mtambalika <karanlicka@gmail.com>
2026-07-21 09:51:29 +0530

    the logic is incorrect

--- Python-Bootcamp/dsa/leetcode_128.py
+++ Python-Bootcamp/dsa/leetcode_128.py
@@ -1,7 +1,9 @@
-nums = [0,3,7,2,5,8,4,6,0,1]
+nums = [1, 2, 5, 8, 9]
 
 set_nums = set(nums)
 sequence = set()
+if len(set_nums) == 1:
+    print(len(set_nums))
 
 for num in nums:
     if (num - 1 in nums) or (num + 1 in nums):
