c30de696835ac6499009d9ad9fc55f7c3a271511
Alfred Mtambalika <karanlicka@gmail.com>
2026-08-11 05:59:40 +0530

    managing context

--- Python-Bootcamp/python-basics/sliding_window.py
+++ Python-Bootcamp/python-basics/sliding_window.py
@@ -3,7 +3,19 @@ s1 = 'ab'
 s2 =  "caaabx"
 
 left = 0
-right = len(s1)-1
+right = len(s1)
 
 print(left)
 print(right)
+
+window = []
+
+for left in range(len(s2)):
+    window.append(s2[left:right])
+    print(window)
+    print(left)
+
+    right += 1
+
+
+
