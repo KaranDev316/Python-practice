25b76bc218c7fe18a979187ee59cf0600338600e
Alfred Mtambalika <karanlicka@gmail.com>
2026-08-18 03:56:19 +0530

    wrong approach, the question involves use of prefix sum and hashmap, sliding window alone will fail when other elements are negative

--- Python-Bootcamp/dsa/leetcode_560.py
+++ Python-Bootcamp/dsa/leetcode_560.py
@@ -2,10 +2,22 @@ nums = [1,2,3,1]
 k = 3
 count  =  0
 sum = 0
+left = 0
+right = 0
 
-for right in range(len(nums)):
-    sum += nums[right]
-    print(sum)
+for i in range(len(nums)):
+    sum += nums[left]
+    if sum == k:
+        count += 1
+    while right < len(nums):
+        sum += nums[right]
+        if sum == k:
+            count += 1
+
+        right += 1
+    if left == right:
+        left += 1
+
     if sum == k:
         count += 1
 
